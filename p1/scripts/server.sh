db65a6f9f920c35064089ac5dc8a89df718982a9
Aoniii <aoniii.contact@gmail.com>
2026-05-21 00:08:19 +0200

    perf: improve command performance in setup script

--- p1/scripts/server.sh
+++ p1/scripts/server.sh
@@ -3,7 +3,7 @@
 apt-get update
 apt-get install -y curl
 
-export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip 192.168.56.110 --bind-address 192.168.56.110 --advertise-address 192.168.56.110"
+export INSTALL_K3S_EXEC="server --write-kubeconfig-mode 644 --node-ip 192.168.56.110 --bind-address 192.168.56.110 --advertise-address 192.168.56.110 --disable traefik --disable metrics-server"
 
 # Install K3s in server (controller) mode
 curl -sfL https://get.k3s.io | sh -
